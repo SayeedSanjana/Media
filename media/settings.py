2b283effacf059b6d9ac1ac0137639ec7041c00c
SayeedSanjana <sanjanasayeed68@gmail.com>
2020-06-11 03:06:24 +0600

    initial blog page

--- media/settings.py
+++ media/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
